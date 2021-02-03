26fc3f1c5c8439aba48716a67b59232ac57ea96a
Haramrit Khurana <haramrit09k@gmail.com>
2021-02-03 02:43:19 -0500

    Updates

---
+++ precache-manifest.02abda8747758a2af0e926f8aa900dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd764e76b6c91e963f237d5c3950892",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9437475755cc258cd2c3",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a4e0726449e910756f37",
+    "url": "/static/js/2.1d2dce13.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.1d2dce13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9437475755cc258cd2c3",
+    "url": "/static/js/main.50fea65e.chunk.js"
+  },
+  {
+    "revision": "a47126a1c49d7af7aa53",
+    "url": "/static/js/runtime-main.ff0d576d.js"
+  }
+]);
