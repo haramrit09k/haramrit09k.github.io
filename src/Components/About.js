7311633894ccde26d5097373f51e59dc328b6c0d
Haramrit Khurana <haramrit09k@gmail.com>
2021-04-15 18:42:03 -0400

    added github-profile-card

--- src/Components/About.js
+++ src/Components/About.js
@@ -22,28 +22,29 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Haramrit Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
             <p dangerouslySetInnerHTML={{__html: sanitizer(bio)}}></p>
             <div className="row">
-               <div className="columns contact-details">
+               <div className="columns contact-details download">
                   <h2>Contact Details</h2>
                   <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
+						   <span><h4 className="contact-heading">Address:</h4>{street}<br />
 						         {city} {state}, {zip}
                    </span><br />
-						   <span>{phone}</span><br />
-                     <span><a href="mailto:haramrit09k@gmail.com">{email}</a></span>
+						   <span><h4 className="contact-heading">Phone:</h4>{phone}</span><br />
+                     <span><h4 className="contact-heading">Email:</h4><a href="mailto:haramrit09k@gmail.com">{email}</a></span>
 					   </p>
-               </div>
-               <div className="columns download">
                   <p>
                      <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
+               </div>
+               <h2 className="github-stats-heading">~My GitHub Stats~</h2>
+               <div className="columns github-profile">
+               <github-profile-widget username="haramrit09k"></github-profile-widget>
                </div>
             </div>
          </div>
