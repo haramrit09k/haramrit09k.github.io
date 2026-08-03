b341c9ccadd760c74873911da5595bd32e6bc35b
Haramrit Khurana <haramrit09k@gmail.com>
2026-08-02 20:17:53 -0500

    security: replace vulnerable build toolchain

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-unregister();
